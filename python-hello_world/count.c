6c63fd64c70266ccfcb4d77807ee6e46b9d9fd84
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-05-31 19:41:44 -0700

    12

--- python-hello_world/count.c
+++ python-hello_world/count.c
@@ -2,12 +2,30 @@
 int main(void)
 {
     int i;
-    char *str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax";
+    char *str = "The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!";
 
     for (i = 0; str[i] != '\0'; i++)
     {
-        if (str[i] == 'w' && str[i + 1] == 'i')
+        if (str[i + 1] == '\0')
         {
             printf("%d\n", i);
         }
