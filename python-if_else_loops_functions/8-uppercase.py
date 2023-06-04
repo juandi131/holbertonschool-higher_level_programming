3460a6c06ef7b77a6400a844bdaa47ed4bebca1a
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-04 16:43:03 -0700

    12

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for c in str:
+def uppercase(s):
+    for c in s:
         ascii_val = ord(c)
         if ascii_val >= 97 and ascii_val <= 122:
             upper_ascii = ascii_val - 32
@@ -8,3 +7,10 @@ def uppercase(str):
         else:
             print("{}".format(c), end="")
     print()
+
+def main():
+    string = "lol"
+    uppercase(string)
+
+if __name__ == "__main__":
+    main()
