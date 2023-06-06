b7468220cb84f81bd28d49d19ef738a13877b908
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-06 10:35:16 -0700

    12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-def fizzbuzz():
-    for i in range(1, 101):
-        a = i % 3
-        if a == 0:
-            print("fizz ", end="")
-        else:
-            print(f"{i} ", end="")
-    print()
+for i in range(1, 101):
+    a = i % 3
+    d = i % 5
+    if a == 0 and d == 0:
+        print("FizzBuzz ", end="")
+    elif a == 0:
+        print("Fizz ", end="")
+    elif d == 0:
+        print("Buzz ", end="")
+    else:
+        print(f"{i} ", end="")
+print()
