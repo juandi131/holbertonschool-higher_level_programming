a7667e055990831c1e5b3bd3b4a67fbcdabed6a3
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-10 09:16:26 -0700

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list = [1, 2, 5, 7, 8]
-    idx = 3
-    element = 0
     new_list = []
     if idx >= len(my_list) or idx < 0:
-        return my_list
+        return my_list.copy()
     a = 0
     while a <= (len(my_list) - 1):
         new_list.append(my_list[a])
