01689b935387f56a913eb44fdffd7e963074982d
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-11 13:02:35 -0700

    12

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    char_list = list(my_string) 
-    for i in range(len(my_string)):
-        if my_string[i] == 'C' or my_string[i] == 'c':
-            my_string[i] == ""
-        new_string = "".join(char_list)
-    return my_string
+    new_string = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
