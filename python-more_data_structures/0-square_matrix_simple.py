5cd1dcdffdfbacec2a3982775fb7c3391478d773
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-11 17:58:21 -0700

    12

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix2 = []
-    matrix3 = []
-    for i in matrix:
-        linea = []
-        for j in i:
-            linea.append(j)
-        matrix2.append(linea)
-    for i in matrix2:
-        linea2 = []
-        for j in i:
-            linea2.append(j ** 2)
-        matrix3.append(linea2)
-    print(matrix3)
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
