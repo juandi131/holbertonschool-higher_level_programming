1c74a7b82b81aadf65195894a52028179b5ac429
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-13 16:22:14 -0700

    12

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    nexo = list(map(lambda i: list(map(lambda x: x * x, i)), matrix))
-    return(nexo)
+def square_matrix_simple(matrix):
+    new_matrix = []
+    for row in matrix:
+        new_row = [x ** 2 for x in row]
+        new_matrix.append(new_row)
+    return new_matrix
