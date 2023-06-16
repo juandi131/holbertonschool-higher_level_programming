5af5b6dc1370e9fd837574d20f341ec0f539b3fa
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-16 13:49:38 -0700

    ñ12

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-my_dictionary = {
-    "nombre": "Juan",
-    "edad": 25,
-    "ciudad": "Madrid",
-    "profesion": "Ingeniero",
-    "hobbies": ["leer", "correr", "tocar la guitarra"]
-}
-
-print(f"{my_dictionary}")
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary
