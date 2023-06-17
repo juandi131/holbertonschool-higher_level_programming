1894694178cbfb157aa91f3f85069af73eabf0fe
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-17 11:49:46 -0700

    12

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary:
+        best = 0
+        str = [""]
+        for elements in a_dictionary:
+            if best < a_dictionary[elements]:
+                best = a_dictionary[elements]
+                str = elements
+        return str
+    else:
         return None
-    values = list(a_dictionary.values())
-    b = len(values) - 1
-    for a in range(b):
-        best = values[a]
-        if values[a] < values[a + 1]:
-            best = values[a + 1]
-    for key in a_dictionary:
-        if a_dictionary[key] == best:
-            return(key)
