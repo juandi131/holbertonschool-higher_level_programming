f755787fc240babae1b8f896758e9613d39f963e
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-18 13:13:34 -0700

    12

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" cuadrado cuadrado"""
+
+
 class Squere:
+    """ texto texto"""
+    
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
