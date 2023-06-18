f1bce399f57649fc094b2213e9c144b14f98d656
juandi131 <124542502+juandi131@users.noreply.github.com>
2023-06-18 14:20:00 -0700

    12

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" cuadrado cuadrado"""
+
+
+class Square:
+    """ texto texto"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """ comentario"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ comentario """
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ area de cuadradp """
+        return self.__size ** 2
