231635bd1216d2e93c15e4c06eda03a0d06a9cdf
juandi131 <juandigarcia3@gmail.com>
2023-09-24 14:33:31 -0700

    12

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
-str = "seXonFus"
-for letter in str:
-    if ord(letter) >= 97 and ord(letter) <= 122:
-        letter = chr(ord(letter) - 32)
-    print(f"{letter}", end="")
-print("")
+def print_last_digit(number):
+    if number > 0:
+        number = number % 10
+    elif number < 0:
+        number = number % -10
+    return(number)
