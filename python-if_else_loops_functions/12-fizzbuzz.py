09cba8b153ed319794792cbdc8c3c9747eb2feeb
juandi131 <juandigarcia3@gmail.com>
2023-09-24 15:10:24 -0700

    12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-#def fizzbuzz():
-for a in range(101):
-    if a % 3 == 0:
-        print("sexo")
-    else:
-        print(a)
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif i % 3 == 0:
+            print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(i), end="")
