e93d148911bd6f15ccd40382796ce52f9a86ba96
juandi131 <juandigarcia3@gmail.com>
2023-09-24 10:21:19 -0700

    12

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if lastnumber > 5:
     print(f" is {lastnumber} and is greater than 5")
 elif lastnumber < 6:
     print(f" is {lastnumber} and is less than 6 and not 0")
-else:
-    print(f" is {lastnumber} and is 0")
+elif number == 0:
+    print(f" is 0 and is 0")
