9d9253476299ebbb7d4899a7595b5c57476f9432
juandi131 <juandigarcia3@gmail.com>
2023-09-24 17:22:15 -0700

    12

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
-    n = 0
-    resultado = 1
-    if len(argv) == 1:
-        print("0")
-    for a in argv:
-        if n == 0:
-            n += 1
-            continue
-        resultado = resultado + int(a)
+    resultado = 0
+    for a in argv[1:]:
+        resultado += int(a)
+
     print("{}".format(resultado))
