0300cefb19c5b048f28fd54a897084c38716be64
juandi131 <juandigarcia3@gmail.com>
2023-09-24 17:28:40 -0700

    12

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    n = 0
-    i = 0
+    a = 0
+    s = 0
     for nums in argv:
-        if i != 0:
-            n += int(nums)
-        i = 1
-    print("{}".format(n))
-    
+        if s != 0:
+            a += int(nums)
+        s = 1
+    print("{}".format(a))
