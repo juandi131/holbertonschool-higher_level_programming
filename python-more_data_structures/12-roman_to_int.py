986bc0131712de97a14286ffa7f12f83d31e551d
juandi131 <juandigarcia3@gmail.com>
2023-09-30 17:37:47 -0700

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return None
-    resultado = 0
-    cadena = []
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    
     roman_dict = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-
-    # Convertir números romanos a una lista de valores
-    for char in cesar:
-        if char in roman_dict:
-            valor = roman_dict[char]
-            cadena.append(valor)
-
-    # Calcular el resultado sumando los valores
-    for i in range(len(cadena)):
-        if i < len(cadena) - 1 and cadena[i] < cadena[i + 1]:
-            resultado -= cadena[i]
+    
+    result = 0
+    prev_value = 0
+    
+    for symbol in reversed(roman_string):
+        value = roman_dict[symbol]
+        
+        if value < prev_value:
+            result -= value
         else:
-            resultado += cadena[i]
-
-    print(resultado)
+            result += value
+        
+        prev_value = value
     
