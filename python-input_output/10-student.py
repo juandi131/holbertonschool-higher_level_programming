a7aea0e94e1ede7fa1825f049496f4c1a7142b65
juandi131 <juandigarcia3@gmail.com>
2023-10-08 19:45:19 -0700

    ejercicio

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" student """
+
+
+class Student:
+    """  student """
+    def __init__(self, first_name, last_name, age):
+        """ init it """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            new_dic = {}
+            for tt in self.__dict__:
+                if tt in attrs:
+                    new_dic[att] = self.__dict__.get(att)
+            return new_dic
+        else:
+            return self.__dict__
+    
