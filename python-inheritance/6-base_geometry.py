dc90aff2b9e3d94b391e4dee7af55ab14c9b0eda
juandi131 <juandigarcia3@gmail.com>
2023-10-08 20:00:13 -0700

    ejercicio

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" base geo """
+
+
+class BaseGeometry:
+    """ base geo """
+    def area(self):
+        """ area of """
+        raise Exception("area() is not implemented")
+    
