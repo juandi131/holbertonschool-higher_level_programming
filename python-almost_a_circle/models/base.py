26f1283f3f33fbd88731fe20113ecf22345c8bbd
juandi131 <juandigarcia3@gmail.com>
2023-10-13 11:37:45 -0700

    ejercicio

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 class Base():
+    """model base"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
+        else:
+            self.id = id
